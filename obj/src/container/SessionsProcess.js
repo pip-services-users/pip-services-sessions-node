c109ee646e2308fc4a297a6c4530d212ec66a311
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 13:55:26 +0300

    added DefaultRpcFactory

--- obj/src/container/SessionsProcess.js
+++ obj/src/container/SessionsProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const SessionsServiceFactory_1 = require("../build/SessionsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class SessionsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("sessions", "User sessions microservice");
         this._factories.add(new SessionsServiceFactory_1.SessionsServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.SessionsProcess = SessionsProcess;
--- src/container/SessionsProcess.ts
+++ src/container/SessionsProcess.ts
@@ -2,11 +2,13 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { SessionsServiceFactory } from '../build/SessionsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class SessionsProcess extends ProcessContainer {
 
     public constructor() {
         super("sessions", "User sessions microservice");
         this._factories.add(new SessionsServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 }
