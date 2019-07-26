a4aa0d1ff2571acecc42fb3653f62a4046e8b04d
pipdeveloper <pipdevs@gmail.com>
2019-07-26 21:47:00 +0000

    project build by Travis CI [skip ci]

--- obj/src/persistence/SessionsMongoDbPersistence.d.ts
+++ obj/src/persistence/SessionsMongoDbPersistence.d.ts
@@ -5,7 +5,7 @@ import { SessionV1 } from '../data/version1/SessionV1';
 import { ISessionsPersistence } from './ISessionsPersistence';
 export declare class SessionsMongoDbPersistence extends IdentifiableMongoosePersistence<SessionV1, string> implements ISessionsPersistence {
     constructor();
-    private composeFilter;
+    private composeFilter(filter);
     getPageByFilter(correlationId: string, filter: FilterParams, paging: PagingParams, callback: any): void;
     create(correlationId: string, item: SessionV1, callback?: (err: any, item: SessionV1) => void): void;
     update(correlationId: string, item: SessionV1, callback?: (err: any, item: SessionV1) => void): void;
--- obj/src/services/version1/SessionsGrpcServiceV1.d.ts
+++ obj/src/services/version1/SessionsGrpcServiceV1.d.ts
@@ -4,13 +4,13 @@ export declare class SessionsGrpcServiceV1 extends GrpcService {
     private _controller;
     constructor();
     setReferences(references: IReferences): void;
-    private getSessions;
-    private getSessionById;
-    private openSession;
-    private storeSessionData;
-    private updateSessionUser;
-    private closeSession;
-    private closeExpiredSessions;
-    private deleteSessionById;
+    private getSessions(call, callback);
+    private getSessionById(call, callback);
+    private openSession(call, callback);
+    private storeSessionData(call, callback);
+    private updateSessionUser(call, callback);
+    private closeSession(call, callback);
+    private closeExpiredSessions(call, callback);
+    private deleteSessionById(call, callback);
     register(): void;
 }
