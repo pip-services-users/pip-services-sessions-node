2afb74a1489db39fa988a101563d8fa15db3f699
pipdeveloper <pipdevs@gmail.com>
2019-07-29 16:35:10 +0000

    project build by Travis CI [skip ci]

--- obj/src/index.js
+++ obj/src/index.js
@@ -9,5 +9,5 @@ __export(require("./logic"));
 __export(require("./services/version1"));
 __export(require("./build"));
 __export(require("./container"));
-//module.exports = require('./container/SessionsSenecaPlugin');
+//module.exports = require('./container/SessionsSenecaPlugin'); 
 //# sourceMappingURL=index.js.map
